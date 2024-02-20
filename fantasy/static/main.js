f789016165616a1a75ce73a88f2e5172b0338261
Alexandre Debortoli <debortolidesouzaa@gmail.com>
2024-02-19 21:11:34 -0300

    feat: add navbar `active` page styling

--- fantasy/static/main.js
+++ fantasy/static/main.js
@@ -1,1 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function () {
+  var path = window.location.pathname.substring(1);
+
+  var navLinks = document.querySelectorAll(".nav-link");
+  navLinks.forEach((link) => {
+    var href = link.innerHTML.toLowerCase();
+
+    if (
+      (href === "home" && path === "") ||
+      (href === "log in" && path === "login") ||
+      (href === "register" && path === "register")
+    ) {
+      link.classList.add("active");
+      link.setAttribute("aria-current", "page");
+    } else {
+      link.classList.remove("active");
+      link.removeAttribute("aria-current");
+    }
+  });
+});
